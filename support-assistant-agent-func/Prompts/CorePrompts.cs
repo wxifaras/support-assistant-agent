8ca98343fe9453af76ebadc1f3849652a8638186
William Xifaras <wxifaras@outlook.com>
2025-03-12 14:48:35 -0400

    wording

--- support-assistant-agent-func/Prompts/CorePrompts.cs
+++ support-assistant-agent-func/Prompts/CorePrompts.cs
@@ -10,7 +10,7 @@ public static class CorePrompts
             articles which describe the known issues along with possible workarounds and solutions. The data you will receive will contain important
             information including the description of the problem, the summary of the chat between the user who submitted the issue and the tech support
             engineer, the workaround if one exists, and the solution if one exists. You must look through all of this information and clearly respond to
-            the user with a summary of the problem, what what discussed between the user and tech support, as well as any possible workarounds and solutions.";
+            the user with a summary of the problem, what was discussed between the user and tech support, as well as any possible workarounds and solutions.";
     
     public static string GetSummaryPrompt(List<Comment> comments)
     {
